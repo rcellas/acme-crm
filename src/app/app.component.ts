6e6b2c9e0471afdc2a1b3e097fc6707ac4fd2001
rcellas <rocio.cejudo.soronellas@gmail.com>
2024-04-18 14:13:48 +0200

    layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeaderComponent, NavbarComponent,DashboardComponent]
 })
 export class AppComponent {
   title = 'acme-crm';
