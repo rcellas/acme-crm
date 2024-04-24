cc6e5c0263dfd36a46e694eb985edcb89173c509
rcellas <rocio.cejudo.soronellas@gmail.com>
2024-04-24 08:53:54 +0200

    design

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatSidenavModule, MatToolbarModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+sidenav: any;
 
 }
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, MatSidenavModule, MatListModule],
+  imports: [RouterLink, RouterLinkActive, MatSidenavModule,FormsModule, ReactiveFormsModule,MatListModule, MatButtonModule, MatToolbarModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  events: string[] = [];
-  opened: boolean | undefined;
-sidenav: any;
+  options = this._formBuilder.group({
+    bottom: 0,
+    fixed: false,
+    top: 0,
+  });
+  constructor(private _formBuilder: FormBuilder) {}
+
+
 }
